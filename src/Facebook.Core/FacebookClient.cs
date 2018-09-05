336facb738ed37633d9eeaa16ac3753f58ec18d4
Oleg Kuznetsov <proggmatic@outlook.com>
2018-09-05 17:04:05 +0300

    Initial create

---
+++ src/Facebook.Core/FacebookClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Facebook.Api.Client
+{
+    public class FacebookClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public FacebookClient(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<T> GetAsync<T>(string url)
+        {
+            var httpClient = _httpClientFactory.CreateClient(ServiceRegistrationExtensions.HTTP_CLIENT_NAME);
+            var response = await httpClient.GetAsync(url).ConfigureAwait(false);
+
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            T result = JsonConvert.DeserializeObject<T>(content);
+
+            return result;
+        }
+
+        public async Task<T> PostAsync<T>(string url, object parameters)
+        {
+            var httpClient = _httpClientFactory.CreateClient(ServiceRegistrationExtensions.HTTP_CLIENT_NAME);
+            var response = await httpClient.PostAsync(url, new StringContent(JsonConvert.SerializeObject(parameters), Encoding.UTF8, "application/json")).ConfigureAwait(false);
+
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            T result = JsonConvert.DeserializeObject<T>(content);
+
+            return result;
+        }
+
+        public async Task<T> DeleteAsync<T>(string url)
+        {
+            var httpClient = _httpClientFactory.CreateClient(ServiceRegistrationExtensions.HTTP_CLIENT_NAME);
+            var response = await httpClient.DeleteAsync(url).ConfigureAwait(false);
+
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            T result = JsonConvert.DeserializeObject<T>(content);
+
+            return result;
+        }
+    }
+}
---
+++ src/Facebook.Core/ServiceRegistrationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Facebook.Api.Client
+{
+    public static class ServiceRegistrationExtensions
+    {
+        internal const string HTTP_CLIENT_NAME = "FacebookApiHttpClient";
+
+        public static IServiceCollection AddBeelineAPI(this IServiceCollection services)
+        {
+            //if (configuration != null && !string.IsNullOrEmpty(configurationSection))
+            //    services.Configure<BeelineApiConfig>(configuration.GetSection(configurationSection));
+
+            services.AddHttpClient(HTTP_CLIENT_NAME, (serviceProvider, client) =>
+                {
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders
+                        .Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                })
+                .ConfigureHttpMessageHandlerBuilder(config => config.PrimaryHandler = new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate });
+
+            //services.AddHttpClient<BeelineApiHttpClient>()
+            //    .AddHttpMessageHandler(c => new  HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate });
+
+            return services;
+        }
+    }
+}
